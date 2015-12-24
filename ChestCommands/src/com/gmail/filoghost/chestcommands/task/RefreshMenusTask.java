f04ee9ed81b4ff97b2a418b22c6f65b5702c6489
MatteCarra <matte.carra@gmail.com>
2015-12-24 11:18:37 +0100

    Update

--- ChestCommands/src/com/gmail/filoghost/chestcommands/task/RefreshMenusTask.java
+++ ChestCommands/src/com/gmail/filoghost/chestcommands/task/RefreshMenusTask.java
@@ -3,6 +3,7 @@ package com.gmail.filoghost.chestcommands.task;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.InventoryView;
 
 import com.gmail.filoghost.chestcommands.internal.ExtendedIconMenu;
@@ -14,30 +15,29 @@ public class RefreshMenusTask implements Runnable {
 
 	@Override
 	public void run() {
-
 		for (Player player : Bukkit.getOnlinePlayers()) {
-			
-			InventoryView view = player.getOpenInventory();
-			if (view == null) {
-				return;
-			}
-			
-			Inventory topInventory = view.getTopInventory();
-			if (topInventory.getHolder() instanceof MenuInventoryHolder) {
-				MenuInventoryHolder menuHolder = (MenuInventoryHolder) topInventory.getHolder();
-				
-				if (menuHolder.getIconMenu() instanceof ExtendedIconMenu) {
-					ExtendedIconMenu extMenu = (ExtendedIconMenu) menuHolder.getIconMenu();
-					
-					if (extMenu.getRefreshTicks() > 0) {
-						if (elapsedTenths % extMenu.getRefreshTicks() == 0) {
-							extMenu.refresh(player, topInventory);
+			try{
+				InventoryView view = player.getOpenInventory();
+				if (view == null) {
+					return;
+				}
+				Inventory topInventory = view.getTopInventory();
+                InventoryHolder holder = topInventory.getHolder();
+				if (holder instanceof MenuInventoryHolder) {
+					MenuInventoryHolder menuHolder = (MenuInventoryHolder) holder;
+					if (menuHolder.getIconMenu() instanceof ExtendedIconMenu) {
+						ExtendedIconMenu extMenu = (ExtendedIconMenu) menuHolder.getIconMenu();
+						if (extMenu.getRefreshTicks() > 0) {
+							if (elapsedTenths % extMenu.getRefreshTicks() == 0) {
+								extMenu.refresh(player, topInventory);
+							}
 						}
 					}
 				}
-			}
+			}catch (ClassCastException ignored){
+                System.out.println("RefreshMenu task exception by inventory of " + player.getName());
+            }
 		}
-		
 		elapsedTenths++;
 	}
 
